5275edd3c4ea1ddd674765dade86ecf612d064fd
Brian-mula <mulatiwangilabrian@gmail.com>
2023-03-01 15:45:24 -0800

    added blog model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Blogs(models.Model):
+    creater=models.CharField(max_length=50,default="Mulati brian")
+    title=models.CharField(max_length=50)
+    description=models.TextField()
+    created_date=models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.creater
